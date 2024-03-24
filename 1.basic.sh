de54eb55540aa13f1a2c857ed5b32d8941b3a13b
leoatchina <leoatchina@outlook.com>
2024-03-24 12:25:15 +0800

    update

--- 1.basic.sh
+++ 1.basic.sh
@@ -9,12 +9,30 @@ apt install -y sshfs python3-pip net-tools && pip install neovim bat
 
 rm /root/.bashrc
 
+program_exists() {
+    local ret='0'
+    command -v $1 >/dev/null 2>&1 || { local ret='1'; }
+    # fail on non-zero return value
+    if [ "$ret" -ne 0 ]; then
+        return 1
+    fi
+    return 0
+}
+
+if program_exists "fzf"; then
+    echo "fzf executable"
+else
+    echo "[ -f ~/.fzf.bash ] && source ~/.fzf.bash" >> ~/.configrc
+fi
+
+
 if [ -d ~/.leovim ]; then
     cd ~/.leovim && git pull
 else
     git clone https://gitee.com/leoatchina/leovim.git ~/.leovim
 fi
 cd ~/.leovim && bash install.sh all
+
 
 if [ $# -gt 0 ] && [ $1 -gt 104 ]; then
     sed -i 's/^#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
