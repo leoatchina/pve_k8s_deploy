846c267275b28c77c5f0850607f1b18df6325638
taotao <taotao@ingru.ai>
2024-07-11 18:02:47 +0800

    add flannel

--- 3.k8s_cluster.sh
+++ 3.k8s_cluster.sh
@@ -111,15 +111,25 @@ done
 calico () {
     pod_network_cidr=$1
     kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml
-
     wget https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/custom-resources.yaml -O /tmp/custom-resources.yaml
     sed -i "s#192.168.0.0/16#$pod_network_cidr#g" /tmp/custom-resources.yaml
     kubectl apply -f /tmp/custom-resources.yaml
 }
 
-# apply on control ip
-ssh -o StrictHostKeyChecking=no root@$ip "$(declare -f calico); calico $pod_network_cidr"
+flannel () {
+    pod_network_cidr=$1
+    wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml /tmp/kube-flannel.yml
+    sed -i "s#10.244.0.0/16#$pod_network_cidr#g" /tmp/kube-flannel.yml
+    kubectl apply -f /tmp/kube-flannel.yml
+}
 
+
+# apply on control ip, 建立cni 网络
+if [ $# > 0 ] && [ "$1" == "flannel" ]; then
+    ssh -o StrictHostKeyChecking=no root@$ip "$(declare -f flannel); flannel $pod_network_cidr"
+else
+    ssh -o StrictHostKeyChecking=no root@$ip "$(declare -f calico); calico $pod_network_cidr"
+fi
 
 # =============================
 # reboot
