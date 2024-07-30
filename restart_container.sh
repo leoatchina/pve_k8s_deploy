56977e36595273a9675e8aa96fa1527db6a24757
taotao <taotao@ingru.ai>
2024-07-30 17:21:10 +0800

    add restart containerd.sh

---
+++ restart_container.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+bash_path=$(cd "$(dirname "$0")";pwd)
+source $bash_path/util.sh
+
+for id in ${ids[@]}; do
+    ip=$ip_segment.$id
+
+    error "==== restart container related services on $ip ===="
+    sleep 2
+    ssh -o StrictHostKeyChecking=no root@$ip "systemctl restart docker containerd  kubelet"
+done
