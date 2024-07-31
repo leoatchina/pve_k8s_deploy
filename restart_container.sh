ea724b7e5a4ce8b0e82b50823a9a610cba795ff2
taotao <taotao@ingru.ai>
2024-07-31 11:40:24 +0800

    update restart container

--- restart_container.sh
+++ restart_container.sh
@@ -3,12 +3,15 @@
 bash_path=$(cd "$(dirname "$0")";pwd)
 source $bash_path/util.sh
 
+restart_container () {
+    [ -f /bin/docker ] && systemctl restart docker
+    [ -f /bin/containerd ] && systemctl restart containerd 
+    [ -f /bin/kubelet ] && systemctl restart kubelet 
+}
+
 for id in ${ids[@]}; do
     ip=$ip_segment.$id
-
     error "==== restart container related services on $ip ===="
     sleep 2
-    ssh -o StrictHostKeyChecking=no root@$ip "systemctl restart docker"
-    ssh -o StrictHostKeyChecking=no root@$ip "systemctl restart containerd"
-    ssh -o StrictHostKeyChecking=no root@$ip "systemctl restart kubelet"
+    ssh -o StrictHostKeyChecking=no root@$ip "$(declare -f restart_container); restart_container "
 done
